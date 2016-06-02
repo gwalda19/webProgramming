9ce065ecb7fa9bd751c9629028c3c090455f9e9d
david gwalthney <barf2005@yahoo.com>
2016-06-02 18:54:57 -0400

    store working

---
+++ store_depot/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StoreDepot::Application.config.secret_key_base = 'b884ccd82b1fd6f7d8267fa730a33adfba9dca5d871e74f422d5d7e87f7cd419247147e0bf88a0bb549b4e022b00f46a1da8deda7cd06e86b6b473055ebb3ef5'
---
+++ store_depot/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+StoreDepot::Application.config.session_store :cookie_store, key: '_store_depot_session'
