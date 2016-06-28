29349e80dc3a43611c57ea0b0d5a8dbe9dcb04bc
david gwalthney <barf2005@yahoo.com>
2016-06-28 17:23:17 -0400

    fixed spelling, so user can log out

--- store_depot/config/routes.rb
+++ store_depot/config/routes.rb
@@ -5,7 +5,7 @@ StoreDepot::Application.routes.draw do
   controller :sessions do
     get    'login'  => :new
     post   'login'  => :create
-    delete 'logout' => :destory
+    delete 'logout' => :destroy
   end
 
   get "sessions/create"
